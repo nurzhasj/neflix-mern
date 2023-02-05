9438da851b49f8e819e9d73552a9f592195da142
Nurzhas <190103276@stu.sdu.edu.kz>
2023-02-05 10:03:41 +0600

    NodeJs MongoDB Connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 // Express for backend side
 const express = require("express");
+// Mongo for database
+const mongoose = require("mongoose");
+// DotEnv for hiding sensitive data
+const dotenv = require("dotenv");
+
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("DB connection is successful"))
+    .catch((err) => console.log(err));
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
